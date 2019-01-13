767f08924f37c900b38434221fb63f1cd9ad09ca
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-01-13 20:19:10 +0100

    .

--- tools/extract_images.sh
+++ tools/extract_images.sh
@@ -1,1 +1,2 @@
-ffmpeg -i stockholm_1280x768x50x420x578.avi %03d.png
+wget http://www.hpca.ual.es/~vruiz/videos/stockholm_1280x768x50x420x578.avi
+ffmpeg -i stockholm_1280x768x50x420x578.avi -vframes 7 %03d.png
