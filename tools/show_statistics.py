2e16206547a7779d8ddda9f8819e5c33770793f3
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-01-23 17:44:55 +0100

    .

--- tools/show_statistics.py
+++ tools/show_statistics.py
@@ -34,13 +34,13 @@ number_of_pixels = width * height
 components = image.shape[2]
 
 histogram = [None]*components
-for i in range(components):
-    histogram[i] = {}
+for c in range(components):
+    histogram[c] = {}
 
 for y in range(width):
     for x in range(height):
         for c in range(components):
-            val = image[y][x][c]
+            val = image[y,x,c]
             if val not in histogram[c]:
                 histogram[c][val] = 1
             else:
@@ -50,16 +50,28 @@ entropy = [None]*components
 for c in range(components):
     entropy[c] = compute_entropy(histogram[c], number_of_pixels)
     
-max = np.amax(image)
-min = np.amin(image)
-dynamic_range = max - min
+max = [None] * components
+min = [None] * components
+dynamic_range = [None] * components
+mean = [None] * components
+for c in range(components):
+    max[c] = np.amax(image[:,:,c])
+    min[c] = np.amin(image[:,:,c])
+    dynamic_range[c] = max[c] - min[c]
+    mean[c] = np.mean(image[:,:,c])
 
+print("Image: {}".format(args.image))
 print("Width: {}".format(width))
 print("Height: {}".format(height))
 print("Components: {}".format(components))
 print("Number of pixels: {}".format(number_of_pixels))
-print("Max value: {}".format(max))
-print("Min value: {}".format(min))
-print("Dynamic range: {}".format(dynamic_range))
+for c in range(components):
+    print("Max value of component {}: {}".format(c, max[c]))
+for c in range(components):
+    print("Min value of component {}: {}".format(c, min[c]))
+for c in range(components):
+    print("Dynamic range of component {}: {}".format(c, dynamic_range[c]))
+for c in range(components):
+    print("Mean of component {}: {}".format(c, mean[c]))
 for c in range(components):
     print("Entropy of component {}: {}".format(c, entropy[c]))
