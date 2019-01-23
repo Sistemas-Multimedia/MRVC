78024ab4406027e714f3d9df6f7a3e38184f1e6d
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-01-23 23:48:56 +0100

    .

---
+++ tools/sub_32768_128.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+import sys
+
+input = sys.argv[1]
+output = sys.argv[2]
+image = cv2.imread(input, -1)
+print(np.amax(image), np.amin(image))
+image -= (32768-128)
+print(np.amax(image), np.amin(image))
+cv2.imwrite(output, image.astype(np.uint8))
