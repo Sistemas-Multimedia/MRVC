a35c73040b9542c9484327fbba2ca684489da7f4
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-01-24 11:49:58 +0100

    .

--- tools/sub_32768_128.py
+++ tools/sub_32768_128.py
@@ -7,7 +7,19 @@ import sys
 input = sys.argv[1]
 output = sys.argv[2]
 image = cv2.imread(input, -1)
-print(np.amax(image), np.amin(image))
+
+if __debug__:
+    print("Max value at input: {}".format(np.amax(image)))
+    print("Min value at input: {}".format(np.amin(image)))
+
+image = np.clip(image, 32768-128, 32768-128+255)
 image -= (32768-128)
-print(np.amax(image), np.amin(image))
+
+if __debug__:
+    print("Substracting {}".format(32768-128))
+
+if __debug__:
+    print("Max value at output: {}".format(np.amax(image)))
+    print("Min value at output: {}".format(np.amin(image)))
+
 cv2.imwrite(output, image.astype(np.uint8))
