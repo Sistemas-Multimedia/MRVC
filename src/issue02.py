8220e2f90ff66be80cb287434b55f939a3251a52
voyagerx3 <voyagerx3@hotmail.com>
2019-01-29 11:29:50 +0100

    Update issue02.py

--- src/issue02.py
+++ src/issue02.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 import subprocess
 import sys
-"""subprocess.run(["bash","./test_sum_sub.sh","10"])"""
-subprocess.call(["bash", 'test_sum_sub.sh 24'])
+from sys import argv
+from pathlib import Path
 
-subprocess.call("./DWT.py -i /tmp/issues02/0000 -d /tmp/issues02/0000", shell=True)
+if len(sys.argv) == 2:
+    fimg = sys.argv[1]
+    #if (type(fimg) != int):
+    #    fimg = 8
+    print(fimg)
+else:
+    print("Error - Introduce los argumentos correctamente")
+    print('Ejemplo: issue02.py  5')
+
+ 
+scriptextr = './test_sum_sub.sh '+ fimg
+ 
+subprocess.call(scriptextr, shell=True)
+
+   
+#subprocess.call("./DWT.py -i /tmp/issues02/0000 -d /tmp/issues02/0000", shell=True)
