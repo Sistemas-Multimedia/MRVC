33453621a0bc3933a280b77de5a2f9c920e2c9e7
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2019-01-29 20:01:39 +0100

    Deleting high frequency subbands of MCDWT

--- tools/substract_offset.py
+++ tools/substract_offset.py
@@ -4,15 +4,13 @@ import cv2
 import numpy as np
 import sys
 import argparse
-from subprocess import check_call
-from subprocess import CalledProcessError
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
     pass
 
-parser = argparse.ArgumentParser(description = "Adds an offset to an image\n\n"
+parser = argparse.ArgumentParser(description = "Substracts (and clips to [0,255]) an offset to an image\n\n"
                                  "Example:\n\n"
-                                 "  add_offset -i ../sequences/stockholm/000 -o /tmp/000 -f 32640\n",
+                                 "  substract_offset -i ../sequences/stockholm/000 -o /tmp/000 -f 32640\n",
                                  formatter_class=CustomFormatter)
 
 parser.add_argument("-i", "--input",
@@ -29,24 +27,21 @@ args = parser.parse_args()
 input = args.input
 output = args.output
 offset = args.offset
-image = cv2.imread(input, -1).astype(np.uint16)
+
+image = cv2.imread(input, -1)
 
 if __debug__:
     print("Max value at input: {}".format(np.amax(image)))
     print("Min value at input: {}".format(np.amin(image)))
 
-if __debug__:
-    print("Adding {}".format(offset))
+image = np.clip(image, offset, offset+255)
+image -= offset
 
-image += offset
+if __debug__:
+    print("Substracting {}".format(32768-128))
 
 if __debug__:
     print("Max value at output: {}".format(np.amax(image)))
     print("Min value at output: {}".format(np.amin(image)))
 
-cv2.imwrite(output + ".png", image.astype(np.uint16))
-try:
-    check_call("mv " + output + ".png " + output, shell=True)
-except CalledProcessError:
-    print("Exception {}".format(traceback.format_exc()))
-
+cv2.imwrite(output, image.astype(np.uint8))
