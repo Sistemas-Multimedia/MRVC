c32bfeadd1bd24299b26ff909f5e47c6040a6ca4
Rick.HunterEC <voyagerx3@hotmail.com>
2019-01-29 03:38:16 -0500

    add gitignore

--- src/issue02.py
+++ src/issue02.py
@@ -2,4 +2,6 @@
 import subprocess
 import sys
 """subprocess.run(["bash","./test_sum_sub.sh","10"])"""
-subprocess.run(["bash", 'extract_images.sh 10 '])
+subprocess.call(["bash", 'test_sum_sub.sh 24'])
+
+subprocess.call("./DWT.py -i /tmp/issues02/0000 -d /tmp/issues02/0000", shell=True)
--- src/test_sum_sub.sh
+++ src/test_sum_sub.sh
@@ -12,7 +12,7 @@ fi
 
 #wget http://www.hpca.ual.es/~vruiz/videos/stockholm_1280x768x50x420x578.avi --output-document=/tmp/stockholm.avi
 wget http://www.hpca.ual.es/~vruiz/videos/flowergarden_352x288x30x420x250.avi --output-document=/tmp/stockholm.avi
-ffmpeg -i /tmp/stockholm.avi -vframes $1 /tmp/issues02/%03d.png
+ffmpeg -i /tmp/stockholm.avi -vframes 36 /tmp/issues02/%03d.png
 
 cd /tmp/issues02/
 for f in *.png; do
@@ -26,8 +26,10 @@ for f in *.png; do
     #mv $f $base
     mv $f $filen
 done
-#for f in /tmp/issues02/*; do
-#./DWT.py -i $f -d /tmp/000  
-#done
+for f in /tmp/issues02/*; do
+# ./DWT.py -i $f -d /tmp/000  
+echo "./DWT.py -i $f -d $f"
+#echo "./DWT.py -i $f -d /tmp/dwt"
+done
 #./add_32768_128.py /tmp/issues02/003.png /tmp/issues02/003_offset.png
 #./sub_32768_128.py /tmp/issues02/003_offset.png /tmp/issues02/003_original.png
