aeff993f6b026f1fa67f2fb704fdde5259b2ace1
Juan Alberto Llopis <p.guillen100@gmail.com>
2019-02-16 16:49:28 +0100

    Finished contribution script, now it works with the latest update of the main repository (before use this script you must use copy the sequences to the tmp directory).

--- src/WCONTRIBUTION.py
+++ src/WCONTRIBUTION.py
@@ -14,6 +14,34 @@ from MDWT import MDWT
 sys.path.insert(0, "..")
 from src.IO import decomposition
 
+def HHtoBlack(path, N):
+    for i in range(N):
+
+        LH, HL, HH = decomposition.readH(path, "{:03d}".format(i))
+        LL = decomposition.readL(path, "{:03d}".format(i))
+
+        y = math.ceil(HH.shape[0]/2)
+        x = math.ceil(HH.shape[1]/2)
+
+        HH = HH * 0
+        HH[x][y][0] = 255
+        HH[x][y][1] = 255
+        HH[x][y][2] = 255
+
+        decomposition.writeH([LH,HL,HH],"{:03d}".format(i))
+
+def getContribution(path, N):
+    for i in range(N):
+
+        LH, HL, HH = decomposition.readH(path, "{:03d}".format(i))
+        LL = decomposition.readL(path, "{:03d}".format(i))
+
+        '''CALCULATE THE CONTRIBUTION OF EACH SUBBAND'''
+        print("LL Contribution --> {}\n".format(np.sum(LL*LL)/np.sum(HH*HH)))
+        print("HL Contribution --> {}\n".format(np.sum(HL*HL)/np.sum(HH*HH)))
+        print("LH Contribution --> {}\n\n".format(np.sum(LH*LH)/np.sum(HH*HH)))
+
+
 if __name__ == "__main__":
 
     import argparse
@@ -21,11 +49,10 @@ if __name__ == "__main__":
     class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
         pass
     
-#        "  yes | cp -rf ../sequences/stockholm/ /tmp/\n"
     parser = argparse.ArgumentParser(
         description = "Wavelet Contribution Calculator\n\n"
         "Example:\n\n"
-        "  python3 -O ./WCONTRIBUTION.py -d /tmp/stockholm_ -K 5 \n",
+        "  python3 -O ./WCONTRIBUTION.py -d /tmp/stockholm_ \n",
         formatter_class=CustomFormatter)
 
     parser.add_argument("-d", "--decompositions",
@@ -37,40 +64,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
     path = os.path.dirname(args.decompositions)
 
-    '''PRIMERA TRANSFORMACIÓN DIRECTA'''
+    '''FIRST DIRECT TRANSFORM'''
     d = MDWT()
-    d.forward('../sequences/stockholm/', args.decompositions, args.N)
+    d.forward(args.decompositions, args.N)
 
-    '''MODIFICAMOS LA SUBBANDA HH PONIENDOLA EN NEGRO CON UN PUNTO BLANCO EN EL CENTRO'''
-    for i in range(args.N):
-
-        LH, HL, HH = decomposition.readH("{}{:03d}".format(args.decompositions, i))
-        LL = decomposition.readL("{}{:03d}".format(args.decompositions, i))
-
-        y = math.ceil(HH.shape[0]/2)
-        x = math.ceil(HH.shape[1]/2)
-
-        HH = HH * 0
-        HH[x][y][0] = 255
-        HH[x][y][1] = 255
-        HH[x][y][2] = 255
-
-        decomposition.writeH([LH,HL,HH],"{}{:03d}".format(args.decompositions, i))
-
+    '''MODIFY THE SUBBAND HH, WHITE DOT IN THE MIDDLE OF THE IMAGE AND THE REST OF PIXELS TO BLACK'''
+    HHtoBlack(args.decompositions, args.N)
    
 
-    '''RECONSTRUIMOS LA IMAGEN Y VOLVEMOS A DESCOMPONERLA PARA CALCULAR LA GANANCIA'''
-    d.backward(args.decompositions, '/tmp/recons_MDWT_', args.N)
-    d.forward('/tmp/recons_MDWT_', args.decompositions, args.N)
+    '''RECONSTRUCT THE IMAGE AND ANOTHER DECOMPOSITION TO GET THE CONTRUBITON OF EACH SUBBAND'''
+    d.backward(args.decompositions, args.N)
+    d.forward(args.decompositions, args.N)
 
-    '''OBTENEMOS LA GANANCIA DE CADA SUBBANDA'''
-    for i in range(args.N):
-
-        LH, HL, HH = decomposition.readH("{}{:03d}".format(args.decompositions, i))
-        LL = decomposition.readL("{}{:03d}".format(args.decompositions, i))
-
-
-        '''CALCULAR GANANCIA DE CADA SUBBANDA'''
-        print("JLL --> {}\n".format(np.sum(LL*LL)/np.sum(HH*HH)))
-        print("JHL --> {}\n".format(np.sum(HL*HL)/np.sum(HH*HH)))
-        print("JLH --> {}\n\n".format(np.sum(LH*LH)/np.sum(HH*HH)))
+    '''GET THE CONTRUBITON OF EACH SUBBAND'''
+    getContribution(args.decompositions, args.N)
