440e129e8d8f8076d7e39c7a6db9cd49972e9934
Viktor Sobolyev <vs613@inlumine.ual.es>
2020-02-12 11:39:15 +0100

    testing script v0.3

--- src/mcdwt_evaluate.py
+++ src/mcdwt_evaluate.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python3
+#title           :mcdwt_evaluate.py
+#description     :script for evaluation of mcdwt algorythm from repository: https://github.com/Sistemas-Multimedia/MCDWT
+#author          :Nabel Hernandez Margin,
+#                 Ángeles Garcia de las Heras,
+#                 Manuel Gonzalez Rodriguez,
+#                 Jose Carlos Fuentes Angulo,
+#                 Viktor Sobolyev
+#date            :06022020
+#version         :0.3
+#notes           :Script intended to be used on a linux machine. Not compatible with any Windows OS
+#python_version  :3.5.1
+#==============================================================================
 import shutil
 import wget
 import sys
@@ -11,6 +24,11 @@ MIDRISE = 'midrise'
 MIDTHREAD = 'midthread'
 DEADZONE = 'deadzone'
 QUANTIZIERS = [MIDRISE, MIDTHREAD, DEADZONE]
+HH = 'HH'
+HL = 'HL'
+LH = 'LH'
+LL = 'LL'
+SUBBANDS = [HH, HL, LH, LL]
 N_ITERATIONS = 0
 VIDEOFILE_NAME = 'test_video'
 VIDEOFILE_URL = ''
@@ -18,7 +36,11 @@ DEFAULT_DIR = '/tmp/mcdwt/'
 TMP_DIR = ''
 LOOP_STEP = 1
 POWER_OF_TWO = True
+DEBUG = False #__debug__
 
+def my_print(msg):
+   if DEBUG:
+      print(msg)
 
 def do_cleanup(tmp_dir):
    '''
@@ -31,7 +53,7 @@ def do_cleanup(tmp_dir):
    -------
    none
    '''
-   print('Limpiando directorio de trabajo: %s' % tmp_dir)
+   my_print('Limpiando directorio de trabajo: %s' % tmp_dir)
    if (os.path.exists(tmp_dir)):
       try:
          shutil.rmtree(tmp_dir)
@@ -52,9 +74,9 @@ def download_file_to_folder(videofile_url, videofile_name):
    -------
 
    '''
-   print('Descargando archivo: \'%s\' a \'%s\'' % (videofile_url, videofile_name))
+   my_print('Descargando archivo: \'%s\' a \'%s\'' % (videofile_url, videofile_name))
    res = wget.download(videofile_url, out=videofile_name)
-   print('\n')
+   my_print('\n')
    return res
 
 
@@ -71,7 +93,7 @@ def extract_frames(tmp_dir, gop, videofile):
    -------
    base directory with original images
    '''
-   print('Extrayendo %s imagenes a %s' % (str(gop), tmp_dir))
+   my_print('Extrayendo %s imagenes a %s' % (str(gop), tmp_dir))
    # ffmpeg -hide_banner -loglevel info -i $file -vframes 9 $folder%03d.png &
    base_dir = tmp_dir + str(gop) + os.sep
    pattern_dir = base_dir + 'original' + os.sep
@@ -108,7 +130,7 @@ def execute_mdwt(image_directory, gop, inverse=False):
    -------
    POPEN object
    '''
-   print('Ejecutando MDWT.py para %s imagenes inverse: %s' %(str(gop), str(inverse)))
+   my_print('Ejecutando MDWT.py para %s imagenes inverse: %s' %(str(gop), str(inverse)))
    # MDWT.py -N $num_imgs -p /tmp/
    args = ['python3', '-O', 'MDWT.py', '-N', str(gop), '-p', image_directory]
    if inverse == True: args.append('-b')
@@ -129,7 +151,7 @@ def execute_mcdwt(image_directory, gop, n_iteration, predictor=1, inverse=False)
    -------
    POPEN object
    '''
-   print('Ejecutando MCDWT.py para imagenes %s, predictor %s, iteraciones %s inverse: %s' %(str(gop), str(predictor),
+   my_print('Ejecutando MCDWT.py para imagenes %s, predictor %s, iteraciones %s inverse: %s' %(str(gop), str(predictor),
                                                                                             str(n_iteration), str(inverse)))
    # !python3 -O MCDWT.py -N $num_imgs -P $predictor -p /tmp/ -T $iterations
    args = ['python3', '-O', 'MCDWT.py', '-N', str(gop - 1), '-P', str(predictor), '-p', image_directory, '-T',
@@ -159,9 +181,20 @@ def execute_command(args, cmd=None, shell=False):
    return p
 
 
-def get_dirsize(dir_path, suffix="B"):
-   res = int(check_output(['du', '-sb', dir_path]).split()[0].decode('utf-8'))
-   print('sizeof: %s: %s' % (dir_path, str(res)))
+def get_pathsize(path, suffix="B"):
+   '''
+   return dir/file size
+   Parameters
+   ----------
+   path - file or folder path
+   suffix - result size suffix ['B','MB','KB','GB','TB']
+
+   Returns
+   -------
+
+   '''
+   res = int(check_output(['du', '-sb', path]).split()[0].decode('utf-8'))
+   my_print('sizeof: %s: %s' % (path, str(res)))
    return get_size_format(res, suffix=suffix)
 
 
@@ -212,7 +245,7 @@ def prepare_current_iteration_directory(tmp_dir, curr_gop, gop):
    '''
    srt_dir = tmp_dir + str(gop) + os.sep
    dst_dir = tmp_dir + str(curr_gop) + os.sep
-   print('Copiando %s imagenes al directorio: %s' %(str(curr_gop), dst_dir))
+   my_print('Copiando %s imagenes al directorio: %s' %(str(curr_gop), dst_dir))
    #os.makedirs(tmp_dir, exist_ok=True)
    shutil.copytree(srt_dir, dst_dir)
    #remove extra images
@@ -236,9 +269,9 @@ def quantize_image_function(input_path, output_path, q_step, quantize_algorithm)
    elif quantize_algorithm == MIDTHREAD:
       image = np.round(tmp / q_step).astype(np.int16) * q_step
    elif quantize_algorithm == DEADZONE:
-      image = (tmp / q_step).astype(np.int16).astype(np.int)
+      image = (tmp / q_step).astype(np.int16) * q_step
    else:
-      image = (tmp / q_step).astype(np.int16) * q_step  # Vicente's quantizer.py algorithm
+      image = (tmp / q_step).astype(np.int16) * q_step  # Vicente's quantizer.py algorithm ///-> deadzone
 
    # Transformation needed because Vicente said in forum
    tmp = image.astype(np.float32)
@@ -247,46 +280,6 @@ def quantize_image_function(input_path, output_path, q_step, quantize_algorithm)
 
    # Write image quantizied
    cv2.imwrite(output_path, image)
-
-def prepare_quantization_directories(tmp_dir, q_step=32):
-   '''
-   Copy images from mcdwt result algorithm into 3 directories for quantifier evaluation
-   Parameters
-   ----------
-   dir - string working directory
-   dir_arr - array of dir_names to copy res directory
-   Returns
-   -------
-   array destination directories full paths
-   '''
-   print('PREPARE DIRECTORIES FOR QUANTIZATION')
-   global DEFAULT_DIR
-   if isinstance(tmp_dir, str):
-      if (tmp_dir == ''): tmp_dir = DEFAULT_DIR
-      if (not tmp_dir.endswith(os.sep)):
-         tmp_dir += os.sep
-      if len(QUANTIZIERS) > 0:
-         directories = [name for name in os.listdir(tmp_dir) if os.path.isdir(os.path.join(tmp_dir, name))]
-         res_dirs = []
-         for dir in directories:
-            curr_path = tmp_dir + dir + os.sep
-            for d in QUANTIZIERS:
-               src_dir = curr_path + 'res' + os.sep
-               dst_dir = curr_path + d + os.sep
-               #remove dst_dir if exists
-               cmd = 'rm'
-               args = '-rf %s' % dst_dir
-               execute_command(args, cmd=cmd, shell=True)
-
-               os.makedirs(dst_dir, exist_ok=True)
-               res_dirs.append(dst_dir)
-               print('creating directory: %s' %dst_dir)
-
-         return res_dirs
-      else:
-         raise Exception('dir_arr cannot be empty')
-   else:
-      raise Exception('tmp_dir must be a string')
 
 def mse(path_A, path_B):
    '''
@@ -307,21 +300,61 @@ def mse(path_A, path_B):
    A = cv2.imread(path_A, -1)
    B = cv2.imread(path_B, -1)
    err = np.sum((A - B) ** 2)
-
    err /= float(A.shape[0] * A.shape[1])
 
-   # return the MSE, the lower the error, the more "similar"
-   # the two images are
    return err
-
 
 def func_qstep(qstep):
    if POWER_OF_TWO == True:
       qstep = int(pow(2, qstep))
    return qstep
 
-def quantisize_images(tmp_dir, videofile_url, n_iterations, predictor=2, suffix='B',q_step=None, min_qstep=1, max_qstep=6, normalize=False):
+def quantisize_images(tmp_dir, videofile_url, n_iterations, allsubbands=True, predictor=2, suffix='B', q_step=None, min_qstep=1, max_qstep=6, loopstep=None, powered_iterations=None, normalize=False):
+   '''
+   Runs quantization test for 3 defined algorithms (deadzone, midthread, midrise)
+   Parameters
+   ----------
+   tmp_dir (str): directory for the algorythm to download video, extract images and subbands
+   videofile_url (str): url of the videofile to extract images from
+   n_iterations (int): value of the iterations for mcdwt algorythm
+   predictor (int): value of the predictor for mcdwt algorythm
+   suffix (str): suffix for the size of images, possible values: B, KB, MB, GB, TB
+   q_step (int): start qstep iterations with value 1 and end with value q_step
+   min_qstep (int): start qstep iterations with value
+   max_qstep (int): end qstep iterations with value
+   loopstep (int): value of qstep loop increment, default 1 (1,2,3,4,5...)
+   powered_iterations (boolean) : if set to true, iterations of qstep for the quantisizer will be assigned a value of, qstep = 2^iteration_value
+   normalize (boolean): normalizes images after finishing algorythm execution
+
+   Returns
+   -------
+   json
+   example to extract data from resulting json mse_value:
+   def separate_msevalues_results(mse_values, mse_dz=[], mse_mr=[], mse_mt=[], size_dz=[], size_mr=[], size_mt=[]):
+    # mse arrays
+    # mse_dz = []#deadzone
+    # mse_mr = []#midrise
+    # mse_mt = []#midthread
+
+    # x - q size values
+    # size_dz = []
+    # size_mr = []
+    # size_mt = []
+    for i in range(min_qstep, max_qstep + 1, LOOP_STEP):
+        mse_dz.append(np.mean(mse_values['deadzone'][str(func_qstep(i))]['mse']))
+        size_dz.append(mse_values['deadzone'][str(func_qstep(i))]['size'])
+
+        mse_mr.append(np.mean(mse_values['midrise'][str(func_qstep(i))]['mse']))
+        size_mr.append(mse_values['midrise'][str(func_qstep(i))]['size'])
+
+        mse_mt.append(np.mean(mse_values['midthread'][str(func_qstep(i))]['mse']))
+        size_mt.append(mse_values['midthread'][str(func_qstep(i))]['size'])
+   '''
    global DEFAULT_DIR
+   global POWER_OF_TWO
+   global LOOP_STEP
+   if loopstep is not None: LOOP_STEP = loopstep
+   if powered_iterations is not None: POWER_OF_TWO = powered_iterations
    if min_qstep > max_qstep: min_qstep= max_qstep
    if max_qstep < min_qstep: max_qstep = min_qstep
    if q_step is not None: min_qstep = max_qstep = q_step
@@ -338,12 +371,12 @@ def quantisize_images(tmp_dir, videofile_url, n_iterations, predictor=2, suffix=
 
    # read image names from res directory (compressed)
    img_arr = [name for name in os.listdir(res_dir) if (os.path.isfile(os.path.join(res_dir, name)) and name.endswith('.png'))]
-   print('--------------------------------------------------------------------------------------------')
-   print('cuantificando imagenes arrlength: %s' %(str(len(img_arr))))
-   print('--------------------------------------------------------------------------------------------')
+   my_print('--------------------------------------------------------------------------------------------')
+   my_print('cuantificando imagenes arrlength: %s' %(str(len(img_arr))))
+   my_print('--------------------------------------------------------------------------------------------')
    # quantify images applying q_step 1..max_qstep
    for q_func in QUANTIZIERS:
-      print('cuantificandor: %s ' % (q_func))
+      my_print('cuantificandor: %s ' % (q_func))
       for img_name in img_arr:
          src_image = res_dir + img_name
          #for qstep in range(min_qstep, max_qstep + 1):
@@ -354,9 +387,9 @@ def quantisize_images(tmp_dir, videofile_url, n_iterations, predictor=2, suffix=
             os.makedirs(dst_dir, exist_ok=True)
             dst_image = dst_dir + img_name
             quantize_image_function(src_image, dst_image, qstep, q_func)
-   print('--------------------------------------------------------------------------------------------')
-   print('descopmrimiendo imagenes cuantificadas')
-   print('--------------------------------------------------------------------------------------------')
+   my_print('--------------------------------------------------------------------------------------------')
+   my_print('descopmrimiendo imagenes cuantificadas')
+   my_print('--------------------------------------------------------------------------------------------')
    mse_values = {}
    mse_values[DEADZONE] = {}
    mse_values[MIDTHREAD] = {}
@@ -367,57 +400,77 @@ def quantisize_images(tmp_dir, videofile_url, n_iterations, predictor=2, suffix=
          qstep = func_qstep(qstep)
 
          mse_values[q_func][str(qstep)] = {}
-         mse_values[q_func][str(qstep)]['mse'] = []
-         mse_values[q_func][str(qstep)]['size'] = -1
+         if allsubbands == True:
+            mse_values[q_func][str(qstep)]['mse'] = []
+            mse_values[q_func][str(qstep)]['size'] = -1
+         else:
+            mse_values[q_func][str(qstep)] = {}
+            for sb in SUBBANDS:
+               mse_values[q_func][str(qstep)][sb] = {
+                  'mse': [],
+                  'size': []
+               }
 
-         print('cuantificandor: %s, qstep: %s ' % (q_func, qstep))
+
+         my_print('cuantificandor: %s, qstep: %s ' % (q_func, qstep))
          q_dir = curr_dir + q_func + os.sep + str(qstep).zfill(2) + os.sep
          qres_dir = q_dir + 'res' + os.sep
          qdst_dir = q_dir + 'decompressed' + os.sep
 
-         print('copiando imagenes antes de descomprimir: %s -> %s' %(qres_dir, qdst_dir))
+         my_print('copiando imagenes antes de descomprimir: %s -> %s' %(qres_dir, qdst_dir))
          shutil.copytree(qres_dir, qdst_dir)
 
-         print('--------------------------------------------------------------------------------------------')
-         print('calculando peso del directorio de las subbandas para q_func: %s, qstep: %s' % (q_func, qstep))
+         my_print('--------------------------------------------------------------------------------------------')
+         my_print('calculando peso del directorio de las subbandas para q_func: %s, qstep: %s' % (q_func, qstep))
          #qdst_dir
-         mse_values[q_func][str(qstep)]['size'] = get_dirsize(qdst_dir, suffix='B')
+         if allsubbands == True:
+            mse_values[q_func][str(qstep)]['size'] = get_pathsize(qdst_dir, suffix='B')
+         else:
+            img_arr = [name for name in os.listdir(qres_dir) if (os.path.isfile(os.path.join(qres_dir, name)) and name.endswith('.png'))]
+            for img_name in img_arr:
+               img_A = curr_dir + 'res' + os.sep + img_name
+               img_B = qdst_dir + img_name
+               mse_val = mse(img_A, img_B)
+               size = get_pathsize(img_B)
+               for sb in SUBBANDS:
+                  if img_name.startswith(sb):
+                     mse_values[q_func][str(qstep)][sb]['mse'].append(mse_val)
+                     mse_values[q_func][str(qstep)][sb]['size'].append(size)
 
          #python3 -O MCDWT.py -P $predictor -p /tmp/ -b
          execute_mcdwt(qdst_dir, pow(2, n_iterations), n_iterations, predictor=predictor, inverse=True)
          #python3 -O MDWT.py -p /tmp/ -b
          execute_mdwt(qdst_dir, pow(2, n_iterations), inverse=True)
-         print('eliminando subbandas (rm -rf ?????.png)')
+         my_print('eliminando subbandas (rm -rf ?????.png)')
          cmd = 'rm'
          args = '-rf %s?????.png' %qdst_dir
          execute_command(args, cmd=cmd, shell=True)
-         print('--------------------------------------------------------------------------------------------')
-         print('calculando mse para q_func: %s, qstep: %s' %(q_func, qstep))
+         my_print('--------------------------------------------------------------------------------------------')
+         my_print('calculando mse para q_func: %s, qstep: %s' %(q_func, qstep))
          img_arr = [name for name in os.listdir(qdst_dir) if (os.path.isfile(os.path.join(qdst_dir, name)) and name.endswith('.png'))]
-         print('directorio: %s' %qdst_dir)
-         print(str(img_arr))
+         my_print('directorio: %s' %qdst_dir)
+         my_print(str(img_arr))
 
-         for img_name in img_arr:
-            img_A = curr_dir + 'original' + os.sep + img_name
-            img_B = qdst_dir + img_name
-            mse_val = mse(img_A, img_B)
-            mse_values[q_func][str(qstep)]['mse'].append(mse_val)
-
-         print(str(mse_values[q_func][str(qstep)]))
-         print(str(np.mean(mse_values[q_func][str(qstep)]['mse'])))
+         if allsubbands == True:
+            for img_name in img_arr:
+               img_A = curr_dir + 'original' + os.sep + img_name
+               img_B = qdst_dir + img_name
+               mse_val = mse(img_A, img_B)
+               mse_values[q_func][str(qstep)]['mse'].append(mse_val)
 
          if normalize == True:
-            print('--------------------------------------------------------------------------------------------')
-            print('normalizando imagenes cuantificadas')
-            print('--------------------------------------------------------------------------------------------')
+            my_print('--------------------------------------------------------------------------------------------')
+            my_print('normalizando imagenes cuantificadas')
+            my_print('--------------------------------------------------------------------------------------------')
             img_arr = [name for name in os.listdir(qdst_dir) if (os.path.isfile(os.path.join(qdst_dir, name)) and len(name)<8)]
             for img in img_arr:
                img = qdst_dir + img
                args = ['convert', '-normalize', img, img]
                execute_command(args)
 
-
    return mse_values
+
+
 
 def compress_mcdwt_once(tmp_dir, videofile_url, n_iterations, predictor=1, suffix='B'):
    global DEFAULT_DIR
@@ -451,10 +504,10 @@ def compress_mcdwt_once(tmp_dir, videofile_url, n_iterations, predictor=1, suffi
    execute_command(args, cmd=cmd, shell=True)
 
    # calculate size of directories
-   print('Calculando tamaño de directorios')
+   my_print('Calculando tamaño de directorios')
    original_dir = curr_dir + 'original' + os.sep
-   y = get_dirsize(original_dir, suffix=suffix)
-   y1 = get_dirsize(res_dir, suffix=suffix)
+   y = get_pathsize(original_dir, suffix=suffix)
+   y1 = get_pathsize(res_dir, suffix=suffix)
    y_arr.append(y)
    y1_arr.append(y1)
    return y_arr, y1_arr
@@ -512,10 +565,10 @@ def compress_mcdwt_iterative(tmp_dir, videofile_url, n_iterations, predictor=1, 
       execute_command(args, cmd=cmd, shell=True)
 
       # calculate size of directories
-      print('Calculando tamaño de directorios')
+      my_print('Calculando tamaño de directorios')
       original_dir = curr_dir + 'original' + os.sep
-      y = get_dirsize(original_dir, suffix=suffix)
-      y1 = get_dirsize(res_dir, suffix=suffix)
+      y = get_pathsize(original_dir, suffix=suffix)
+      y1 = get_pathsize(res_dir, suffix=suffix)
       y_arr.append(y)
       y1_arr.append(y1)
    return y_arr, y1_arr
@@ -546,10 +599,6 @@ def main(argv):
             TMP_DIR = DEFAULT_DIR
 
    uncompressed, compressed = compress_mcdwt_iterative(TMP_DIR, VIDEOFILE_URL, N_ITERATIONS)
-
-   print(str(uncompressed))
-   print(str(compressed))
-
 
 if __name__ == "__main__":
    main(sys.argv[1:])
