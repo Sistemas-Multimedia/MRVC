fd8feaa7041f83db2d856d18f9af22a11cfbc400
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-02-01 10:39:57 +0100

    .

--- src/YCoCg.py
+++ src/YCoCg.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 
-def RGB_to_YCoCg(RGB_frame):
+def from_RGB(RGB_frame):
     R, G, B = RGB_frame[:,:,0], RGB_frame[:,:,1], RGB_frame[:,:,2]
     YCoCg_frame = np.empty_like(RGB_frame)
     YCoCg_frame[:,:,0] =  R/4 + G/2 + B/4 
@@ -10,7 +10,7 @@ def RGB_to_YCoCg(RGB_frame):
     YCoCg_frame[:,:,2] = -R/4 + G/2 - B/4
     return YCoCg_frame
 
-def YCoCg_to_RGB(YCoCg_frame):
+def to_RGB(YCoCg_frame):
     Y, Co, Cg = YCoCg_frame[:,:,0], YCoCg_frame[:,:,1], YCoCg_frame[:,:,2]
     RGB_frame = np.empty_like(YCoCg_frame)
     RGB_frame[:,:,0] = Y + Co - Cg 
--- src/hybrid.py
+++ src/hybrid.py
@@ -13,10 +13,10 @@ Q_STEP = 1
 def load_frame(prefix, frame_number):
     ASCII_frame_number = str(frame_number).zfill(3)
     fn = f"{prefix}{ASCII_frame_number}.png"
-    frame = cv2.imread(fn, cv2.IMREAD_UNCHANGED) # [rows, columns, components]
+    frame = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     frame = np.array(frame)
-    return frame
+    return frame # [rows, columns, components]
 
 def write_frame(frame, prefix, frame_number):
     ASCII_frame_number = str(frame_number).zfill(3)
@@ -29,7 +29,7 @@ def read_L(prefix, frame_number):
     subband = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     subband = np.array(subband, dtype=np.float64)
     subband -= 32768.0
-    return subband
+    return subband # [rows, columns, components]
 
 def write_L(L, prefix, frame_number):
     subband = np.array(L, dtype=np.float64)
@@ -46,12 +46,12 @@ def read_H(prefix, frame_number):
     sb = 0
     for sbn in subband_names:
         fn = f"{prefix}{sbn}{ASCII_frame_number}.png"
-        subband = cv2.imread(fn, cv2.IMREAD_UNCHANGED) # [rows, columns, components]
+        subband = cv2.imread(fn, cv2.IMREAD_UNCHANGED)
         subband = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         subband = np.array(subband, dtype=np.float64)
         subband -= 32768.0
         H.append(subband)
-    return H
+    return H # [LH, HL, HH], each one [rows, columns, components]
 
 def write_H(H, prefix, frame_number):
     ASCII_frame_number = str(frame_number).zfill(3)
@@ -87,7 +87,7 @@ def encode(prefix=PREFIX, n_frames=5):
     k = 0
     ASCII_k = str(k).zfill(3)
     V_k = load_frame(prefix, ASCII_k)
-    # RGB -> TCoCg
+    V_k = YCoCg.from_RGB(V_k)
     V_k_L, V_k_H = DWT.analyze(V_k) # (a)
     _V_k_L = interpolate_L(V_k_L) # (b)
     _V_k_1_L = _V_k_L # (c)
@@ -103,18 +103,19 @@ def encode(prefix=PREFIX, n_frames=5):
     for k in range(1, n_frames):
         ASCII_k = str(k).zfill(3)
         V_k = load_frame(prefix, ASCII_k)
+        V_k = YCoCg.from_RGB(V_k)
         V_k_L, V_k_H = DWT.analyze(V_k) # (a)
         _V_k_L = interpolate_L(V_k_L) # (b)
-        flow = motion.estimate(_V_k_L, _V_k_1_L) # (d)
+        flow = motion.estimate(_V_k_L[:,:,0], _V_k_1_L[:,:,0]) # (d)
         prediction__V_k_L = motion.predict(_V_k_1_L, flow) # (e)
-        _V_k_1_L = _V_k_l # (c)
+        _V_k_1_L = _V_k_L # (c)
         _E_k_L = _V_k_L - prediction__V_k_L # (f)
         S_k = _E_k_L < _V_k_L # (g)
         _V_k_H = interpolate_H(V_k_H) # (h)
-        prediction__V_k_H = motion.predict(reconstructed_V_k_1_H, flow) # (n)
-        IP_prediction__V_k_H = np.where(S_k, prediction_V_k_H, _V_k_H) # (?)
-        _E_k_H = _V_k_H - IP_prediction_V_k_H # (i)
-        quantized__E_k_H = deadzone.quantize(E_k_H, q_step=Q_STEP) # (j)
+        prediction__V_k_H = motion.predict(reconstructed__V_k_1_H, flow) # (n)
+        IP_prediction__V_k_H = np.where(S_k, prediction__V_k_H, _V_k_H) # (?)
+        _E_k_H = _V_k_H - IP_prediction__V_k_H # (i)
+        quantized__E_k_H = deadzone.quantize(_E_k_H, q_step=Q_STEP) # (j)
         dequantized__E_k_H = deadzone.dequantize(quantized__E_k_H, q_step=Q_STEP) # (k)
         reconstructed__V_k_H = dequantized__E_k_H + IP_prediction__V_k_H # (l)
         write_H(reconstructed__V_k_H, prefix + "reconstructed_encoder", ASCII_k)
@@ -136,13 +137,14 @@ def decode(prefix=PREFIX, n_frames=5):
     reconstructed__V_k_1_H = reconstructed__V_k_H # (m)
     reconstructed_V_k_H = reduce_H(reconstructed__V_k_H) # (s)
     reconstructed_V_k = DWT.synthesize(V_k_L, reconstructed_V_k_H) # (t)
+    reconstructed_V_k = YCoCg.to_RGB(reconstructed_V_k)
     write_frame(reconstructed_V_k, prefix, ASCII_k)
     for k in range(1, n_frames):
         ASCII_k = str(k).zfill(3)
         V_k_L = read_L(prefix, ASCII_k) # (q)
         quantized_E_k_H = read_H(prefix, ASCII_k) # (q)
         _V_k_L = interpolate_L(V_k_L) # (b)
-        flow = motion.estimate(_V_k_L, _V_k_1_L) # (d)
+        flow = motion.estimate(_V_k_L[:,:,0], _V_k_1_L[:,:,0]) # (d)
         prediction__V_k_L = motion.predict(_V_k_1_L, flow) # (e)
         _V_k_1_L = _V_k_l # (c)
         _E_k_L = _V_k_L - prediction__V_k_L # (f)
@@ -154,6 +156,7 @@ def decode(prefix=PREFIX, n_frames=5):
         reconstructed__V_k_1_H = reconstructed__V_k_H # (m)
         reconstructed_V_k_H = reduce_H(reconstructed_V_k_H) # (s)
         reconstructed_V_k = DWT.synthesize(V_k_L, reconstructed_V_k_H) # (t)
+        reconstructed_V_k = YCoCg.to_RGB(reconstructed_V_k)
         write_frame(reconstructed_V_k, prefix + "reconstructed_decoder", ASCII_k)
 
 encode()
