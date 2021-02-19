bb79a09284ba4b18a6f5497525ef2a8d7ad80256
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-02-19 23:37:26 +0100

    .

--- src/football_experiment_multiresolution.py
+++ src/football_experiment_multiresolution.py
@@ -13,38 +13,37 @@ n_frames = 128
 input_video = "/tmp/football_"
 codestream = "/tmp/football_codestream_"
 output_video = "/tmp/football_decoded_"
-levels = 3
+levels = 2
 
 print("Computing DWT")
 for k in range(n_frames):
     V_k = frame.read(input_video, k)
     V_k = YCoCg.from_RGB(V_k)
     decomposition = DWT.analyze(V_k, levels=levels)
-    L.write(decomposition[0], input_video + "L" + str(l), k)
-    for l in range(levels):
-        H.write(decomposition[l+1], input_video + "H" + str(l), k)
+    L.write(decomposition[0], input_video + f"L{levels}", k)
+    print(len(decomposition))
+    for l in range(levels, 1, -1):
+        print(l-1)
+        H.write(decomposition[l-1], input_video + f"H{l}", k)
 
 print("IPP... encoding")
+
+print(f"Computing SRL {levels}")
 IPP_step.encode(input_video + "L" + str(levels), input_video + "H" + str(levels), codestream, n_frames, q_step)
 for k in range(n_frames):
     reconstruction_V_k_H = frame.read(codestream + "reconstructed_H", k)
     V_k_L = frame.read(input_video + "L" + str(levels), k)
     reduced_reconstruction_V_k_H = H.reduce(reconstruction_V_k_H)
     reconstruction_V_k = DWT.synthesize_step(V_k_L, reduced_reconstruction_V_k_H)
-    frame.write(reconstruction_V_k, codestream + "reconstructed", k)
+    frame.write(reconstruction_V_k, codestream + f"reconstructed{levels}_", k)
+
 for l in range(levels,1,-1):
-    IPP_step.encode(codestream + "reconstructed" + str(l), input_video + "H" + str(l), codestream, n_frames, q_step)
+    print(f"Computing SRL {l+1}")
+    IPP_step.encode(codestream + f"reconstructed{l}" + "_", input_video + f"H{l}", codestream, n_frames, q_step)
+    for k in range(n_frames):
+        reconstruction_V_k_H = frame.read(codestream + "reconstructed_H", k)
+        V_k_L = frame.read(input_video + f"L{l}", k)
+        reduced_reconstruction_V_k_H = H.reduce(reconstruction_V_k_H)
+        reconstruction_V_k = DWT.synthesize_step(V_k_L, reduced_reconstruction_V_k_H)
+        frame.write(reconstruction_V_k, codestream + f"reconstructed{l}_", k)
 
-#n_frames = 16
-#encoder = IPP_step.Encoder(video="/tmp/football_", codestream="/tmp/football_codestream_", q_step=128)
-#for k in range(1, n_frames):
-#    encoder.encode_next_frame()
-
-print("IPP ... Decoding")
-IPP_step.decode(codestream, output_video, n_frames, q_step)
-
-for k in range(n_frames):
-    V_k = frame.read(output_video, k)
-    V_k = YCoCg.to_RGB(V_k)
-    V_k = np.clip(V_k, 0, 255)
-    frame.write(V_k, output_video + "R_", k)
