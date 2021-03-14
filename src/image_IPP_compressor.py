5960f4e36255cbcf01701d64c9b6605cce7a78db
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-03-14 23:41:01 +0100

    .

--- src/image_IPP_compressor.py
+++ src/image_IPP_compressor.py
@@ -8,7 +8,7 @@ import frame
 
 print("IPP... encoding")
 
-for q_step in range(0,1):
+for q_step in range(30,31):
 
     IPP.encode(config.input_video, config.codestream, config.n_frames, q_step, config.subpixel_accuracy)
 
