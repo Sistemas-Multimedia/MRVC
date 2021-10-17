243df7b6f205dc7f818194553457c175afda68c1
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-10-17 06:30:26 +0200

    .

--- src/midtread_quantizer.py
+++ src/midtread_quantizer.py
@@ -7,13 +7,14 @@ name = "mid-tread"
 def quantize(x: np.ndarray, step: float) -> np.ndarray:
     assert step > 0
     k = np.rint(x / step).astype(np.int)  # Quantization indexes
+    #k = np.floor(x / step + 0.5).astype(np.int)  # Quantization indexes
     return k
 
 def dequantize(k: np.ndarray, step: float) -> np.ndarray:
     y = step * k
     return y
 
-def quan_dequan(x: np.ndarray, step:float) -> np.ndarray:
-    k = quantize(x, step)
+def quan_dequan(x: np.ndarray, step:float, dtype=np.uint8) -> np.ndarray:
+    k = quantize(x, step).astype(dtype)
     y = dequantize(k, step)
     return y, k
