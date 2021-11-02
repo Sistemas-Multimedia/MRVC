364e0ada247eda9448d037ff9d937ca33c253492
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-11-02 09:12:29 +0100

    .

--- src/information_PNG.py
+++ src/information_PNG.py
@@ -1,35 +1,12 @@
-''' MRVC/rate.py '''
+'''Information estimation.'''
 
 import numpy as np
 import math
-import debug
-import image_3 as image
+import logging
 import os
 
-def energy(x):
-    return np.sum(x.astype(np.double)*x.astype(np.double))
-
-def average_energy(x):
-    return energy(x)/np.size(x)
-
-def entropy(sequence_of_symbols):
-    value, counts = np.unique(sequence_of_symbols, return_counts = True)
-    probs = counts / len(sequence_of_symbols)
-    n_classes = np.count_nonzero(probs)
-    debug.print("information.entropy: sequence_of_symbols.max() =", sequence_of_symbols.max())
-    debug.print("information.entropy: sequence_of_symbols.min() =", sequence_of_symbols.min())
-    debug.print("information.entropy: n_clases = ", n_classes)
-    #debug.print("information.entropy: probs =", probs)
-
-    if n_classes <= 1:
-        return 0
-
-    _entropy = 0.
-    for i in probs:
-        _entropy -= i * math.log(i, 2)
-    debug.print("information.entropy: _entropy =", _entropy)
-
-    return _entropy
+import information
+import image_3 as image
 
 def PNG_BPP(_image, prefix):
     image.write(_image, prefix, 0)
