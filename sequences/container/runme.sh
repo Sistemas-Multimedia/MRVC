027d4d0ab5921e783d92d95424144044f9a6515c
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2021-12-05 07:08:05 +0100

    .

--- sequences/container/runme.sh
+++ sequences/container/runme.sh
@@ -1,7 +1,8 @@
-FILE=container_cif.y4m
-if test -f "$FILE"; then
-    echo "$FILE exists."
-else
-    wget https://media.xiph.org/video/derf/y4m/container_cif.y4m
-fi
-ffmpeg -i $FILE -start_number 0 /tmp/original_%03d.png
+#!/usr/bin/env bash
+
+input_prefix="https://media.xiph.org/video/derf/y4m/"
+sequence="container_cif.y4m"
+output_prefix="/tmp/original_"
+number_of_frames=16
+
+../extract_frames.sh -i $input_prefix -s $sequence -o $output_prefix -n $number_of_frames
---
+++ sequences/extract_frames.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+input_prefix="https://media.xiph.org/video/derf/y4m/"
+sequence="container_cif.y4m"
+output_prefix="/tmp/original_"
+number_of_frames=16
+
+usage() {
+    #echo $0
+    echo "Extracts the frames of a video sequence"
+    echo "  [-i input prefix ($input_prefix)"
+    echo "  [-s sequence file name ($sequence)"
+    echo "  [-o output prefix ($output_prefix)]"
+    echo "  [-n number of frames to extract ($number_of_frames)]"
+    echo "  [-? help]"
+}
+
+#echo $0: parsing: $@
+
+while getopts "i:s:o:n:?" opt; do
+    case ${opt} in
+        i)
+            input_prefix="${OPTARG}"
+            echo "input prefix =" $input_prefix
+            ;;
+        s)
+            sequence="${OPTARG}"
+            echo "sequence file name =" $sequence
+            ;;
+        o)
+            output_prefix="${OPTARG}"
+            echo "output prefix =" $output_refix
+            ;;
+        n)
+            number_of_frames="${OPTARG}"
+            echo "number of frames to extract =" $number_of_frames
+            ;;
+        ?)
+            usage
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -${OPTARG}" >&2
+            usage
+            exit 1
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument." >&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+if test -f "$sequence"; then
+    echo "$FILE exists. Only extracting ..."
+else
+    echo "Extracting ..."
+    wget $input_prefix/$sequence
+fi
+
+ffmpeg -i $sequence -start_number 0 -frames:v $number_of_frames ${output_prefix}%03d.png
