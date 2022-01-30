7f5e5d4a5f2b7e650f0a0307357c378bae177ef1
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-01-30 18:56:32 +0100

    .

--- sequences/bus/runme.sh
+++
@@ -1,7 +0,0 @@
-FILE=bus_cif.y4m
-if test -f "$FILE"; then
-    echo "$FILE exists."
-else
-    wget https://media.xiph.org/video/derf/y4m/bus_cif.y4m
-fi
-ffmpeg -i $FILE -start_number 0 /tmp/original_%03d.png
--- sequences/bus/show_mb_type.sh
+++
@@ -1,2 +0,0 @@
-# https://github.com/FFmpeg/FFmpeg/blob/918de766f545008cb1ab3d62febe71fa064f8ca7/libavcodec/mpegutils.c#L196
-ffplay -debug mb_type /tmp/1.mp4
